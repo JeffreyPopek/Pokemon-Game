7ccf810b5e8212dd995f59197cba2317186ffcbb
JeffreyPopek <Jeffrey.Popek@gmail.com>
2019-07-09 09:48:55 -0700

    yes

---
+++ android/src/com/pokemongame/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pokemongame.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pokemongame.game.pokemongame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new pokemongame(), config);
+	}
+}
---
+++ desktop/src/com/pokemongame/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pokemongame.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pokemongame.game.pokemongame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new pokemongame(), config);
+	}
+}
